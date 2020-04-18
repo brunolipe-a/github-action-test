6d11eb7370840d297a4bf0cf4b164dff4d6650cd
Bruno Felipe <bfaa1999@gmail.com>
2020-04-18 13:41:41 -0300

    Test Deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Bruno Felipe
+                    Deploy
                 </div>
 
                 <div class="links">
